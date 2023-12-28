0a9aac6545a6c83b4abc087a9c8fe1f5cc6d2a80
rabra <rabira.hierpa@gmail.com>
2023-12-28 21:20:36 +0300

    Add Error component

---
+++ src/app/components/shared/Error.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <h1 className="mx-auto text-2xl">
+      Something went wrong. Try to refresh the page
+    </h1>
+  );
+};
+
+export default Error;
